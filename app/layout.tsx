484f4858c55c31ffb0d69313daff376b0d291b5e
Will Kim <rlagmlckd@gmail.com>
2025-05-06 02:32:10 +0900

    feat: Google Analytics 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "AGAPAO",
@@ -22,6 +23,7 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-JBJYL5H0RR" />
     </html>
   );
 }
