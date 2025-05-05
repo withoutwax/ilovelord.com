49ac7c10cdbcddcc47cde4fe94474e5d323a4123
Will Kim <rlagmlckd@gmail.com>
2025-05-06 02:04:08 +0900

    fix: Map 에러 수정

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,50 +1,52 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
-import Script from 'next/script'
-import { NaverMap as NaverMapType } from "@/types/map";
+import React, { useEffect, useState, useRef } from "react";
+import Script from "next/script";
 
 type Lat = number;
 type Lng = number;
 type Coordinates = [Lat, Lng];
 
-const MAP_ELEMENT_ID = 'naver-maps';
+const MAP_ELEMENT_ID = "naver-maps";
 const COORDINATES: Coordinates = [37.526938, 127.030266];
-const INITIAL_ZOOM = 17
+const INITIAL_ZOOM = 17;
 
 export const NaverMap = () => {
-  const mapRef = useRef<NaverMapType | null>(null);
-
-  const initializeMap = () => {
-        const mapOptions = {
-            center: new window.naver.maps.LatLng(COORDINATES),
-            zoom: INITIAL_ZOOM,
-            minZoom: 9,
-            scaleControl: false,
-            mapDataControl: false,
-            logoControlOptions: {
-                position: naver.maps.Position.BOTTOM_LEFT,
-            },
-        };
-        //새로운 네이버 맵 인스턴스 생성
-        const mapIstance = new window.naver.maps.Map(MAP_ELEMENT_ID, mapOptions);
-        mapRef.current = mapIstance;
-    };
+  const mapRef = useRef<naver.maps.Map | null>(null);
+  const [scriptLoaded, setScriptLoaded] = useState(false);
 
   useEffect(() => {
-//    return () => {
-//      mapRef.current?.destroy();
-//    };
-  }, []);
+    if (!scriptLoaded || typeof window.naver === "undefined") return;
+
+    const mapOptions = {
+      center: new window.naver.maps.LatLng(...COORDINATES),
+      zoom: INITIAL_ZOOM,
+      minZoom: 9,
+      scaleControl: false,
+      mapDataControl: false,
+      logoControlOptions: {
+        position: window.naver.maps.Position.BOTTOM_LEFT,
+      },
+    };
+
+    mapRef.current = new window.naver.maps.Map(MAP_ELEMENT_ID, mapOptions);
+
+    return () => {
+      if (mapRef.current) {
+        mapRef.current?.destroy();
+        mapRef.current = null;
+      }
+    };
+  }, [scriptLoaded]);
 
   return (
     <>
-      <Script src="https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=ruf2pjukxr"
-              strategy="afterInteractive"
-              type="text/javascript"
-              onReady={initializeMap}
-        />
-      <div id={MAP_ELEMENT_ID} className="h-full"></div>
+      <Script
+        src="https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=ruf2pjukxr"
+        strategy="afterInteractive"
+        onLoad={() => setScriptLoaded(true)}
+      />
+      <div id={MAP_ELEMENT_ID} className="h-full w-full" />
     </>
   );
 };
